8aaff03ecfaba4362787e5ac6416e5177e09c573
Matheus Patriota <mpatriota55@gmail.com>
2022-12-05 22:03:32 -0300

    task: Acrescente um novo tipo de bolo: bolo de morango (em inglês, strawberry cake), que custa o dobro de um bolo padrão.

---
+++ bakery/StrawberryCake.java
@@ -0,0 +1,18 @@
+public class StrawberryCake extends Decorator{
+    public StrawberryCake(){
+        super();
+    }
+    public StrawberryCake(Cake cake){
+        super(cake);
+    }
+
+    @Override
+    public String getDescription() {
+        return cake.getDescription() + "Strawberry cake";
+    }
+
+    @Override
+    public int getCost() {
+        return cake.getCost() + (super.getCost()*2);
+    }
+}
