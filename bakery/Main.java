61452980fa0d83a402637877314b527da81bfb67
Matheus Patriota <mpatriota55@gmail.com>
2022-12-05 22:16:44 -0300

    more information

--- bakery/Main.java
+++ bakery/Main.java
@@ -4,6 +4,17 @@
 public class Main {
     public static void main(String[] args) {
         // Create the order
+        /**
+         * Altere o código cliente para adicionar o seguinte a uma Order (pedido) e imprima a
+         * Order:
+         * ○ Bolo de chocolate
+         * ○ Bolo de baunilha com o dizer "PLAIN!"
+         * ○ Bolo de baunilha com granulado com os dizeres "FANCY"
+         * ○ Bolo de morango em várias camadas com granulado duplo e dois dizeres
+         * "One of" e "EVERYTHING" (a saída esperada aqui é: Multi-layered
+         * Strawberry cake with sprinkles with sprinkles with saying "One of" with saying
+         * "EVERYTHING")
+         */
         Order order = new Order();
         order.addCake(new ChocolateCake());
         order.addCake(new SayingCake(new VanillaCake(), "PLAIN!"));
