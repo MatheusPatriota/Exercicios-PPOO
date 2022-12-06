2fe40dbf20f4d5a31a649e99d74be506c5c998f3
Matheus Patriota <mpatriota55@gmail.com>
2022-12-06 20:55:11 -0300

    Oders done

--- Orders/src/Main.java
+++ Orders/src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Composite box = new Box("Encomenda completa", 0.25);
+        box.addComponent(getSurpriseBox());
+
+        System.out.println("Valor Total: " + box.getTotalValue());
+    }
+
+    public static Composite getSurpriseBox() {
+        Composite balls = new Product("Bolinha", 2.50);
+        Composite smartphone_case = new Product("Case", 15.50);
+
+        Composite fish = new Product("Peixe", 22.50);
+        Composite moto = new Product("Moto", 150.50);
+        Composite mini_box = new Box("Mini Box", 15.50);
+        mini_box.addComponent(fish);
+        mini_box.addComponent(moto);
+
+        Composite box = new Box("Encomenda completa", 0.25);
+        box.addComponent(balls);
+        box.addComponent(smartphone_case);
+        box.addComponent(mini_box);
+
+        return box;
     }
 }
